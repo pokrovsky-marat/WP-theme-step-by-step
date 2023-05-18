eaadf3993b0fb8c3682fbe9470d19358724639da
marat <savant.87@mail.ru>
2023-05-18 13:53:23 +0600

    include js in funcitons.php

--- functions.php
+++ functions.php
@@ -6,4 +6,6 @@ function childhood_scripts()
   wp_enqueue_style("childhood-style", get_stylesheet_uri());
   // Ниже правило позволяющее добавить файл стилей из assets или внешнего cdn
   // wp_enqueue_style("header-style", get_template_directory_uri() . "/assets/styles/main.min.css");
+  //Подключаем js, Если наш скрипт зависит от другого скрипта например jquery, то прописываем его в array('jquery')
+  wp_enqueue_script('childhood-script', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
 }
