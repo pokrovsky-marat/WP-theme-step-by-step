306a22886bba5d049433333e84deb8a6016b0c8d
marat <savant.87@mail.ru>
2023-05-18 13:25:25 +0600

    Add css files from other directories or cdn

--- functions.php
+++ functions.php
@@ -4,4 +4,6 @@ add_action("wp_enqueue_scripts", "childhood_scripts");
 function childhood_scripts()
 {
   wp_enqueue_style("childhood-style", get_stylesheet_uri());
+  // Ниже правило позволяющее добавить файл стилей из assets или внешнего cdn
+  // wp_enqueue_style("header-style", get_template_directory_uri() . "/assets/styles/main.min.css");
 }
