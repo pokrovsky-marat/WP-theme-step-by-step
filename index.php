eb7d847732f3fe99ed6a36fb50e0e6d1fc08f798
marat <savant.87@mail.ru>
2023-05-18 12:24:50 +0600

    remove redundant CSS include

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
         <link rel="apple-touch-icon" sizes="120x120" href="./img/favicons/apple-touch-icon-120x120.png">
         <link rel="apple-touch-icon" sizes="144x144" href="./img/favicons/apple-touch-icon-144x144.png">
         <link rel="apple-touch-icon" sizes="180x180" href="./img/favicons/apple-touch-icon-180x180.png">
-        <link rel="stylesheet" href="./styles/main.min.css">
         <?php wp_head(); ?>
       </head>
     <body class="no-svg">
