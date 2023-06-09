bc981c3b8be3416f5b789a722f07df4155d19f7c
marat <savant.87@mail.ru>
2023-06-09 09:32:24 +0600

    acf aboutus block

--- index.php
+++ index.php
@@ -203,52 +203,40 @@
 
 <div class="aboutus" id="aboutus">
     <div class="container">
-        <h1 class="title">Наша история</h1>
+        <h1 class="title"><?php the_field('history_heading', 2) ?></h1>
         <div class="row">
             <div class="col-lg-6">
                 <div class="subtitle">
-                    Все начиналось с желания
+                    <?php the_field('history_sub_1', 2) ?>
                 </div>
-                <div class="aboutus__text">
-                    Желания сделать как можно больше детей счастливыми. Именно с этой идеи все и зарождалось.
-                    <br><br>
-                    Первые игрушки, сделанные вручную были классическими плюшевыми медведями, которые разошлись настолько быстро, что нас завалили заказами на несколько месяцев вперед. Именно в то время мы поняли, что идем правильным путем, вкладывая все силы и эмоции в наши игрушки.
-                </div>
+                <div class="aboutus__text"><?php the_field('history_descr_1', 2) ?></div>
             </div>
             <div class="col-lg-6">
-                <img class="aboutus__img" src="<?php echo bloginfo('template_directory') . "/assets/img" ?>/about_1.jpg" alt="мир детства">
+                <img class="aboutus__img" src="<?php echo get_field('history_img_1', 2)['url']; ?>" alt="<?php echo get_field('history_img_1', 2)['alt']; ?>">
             </div>
         </div>
 
         <div class="row">
             <div class="col-lg-6">
-                <img class="aboutus__img" src="<?php echo bloginfo('template_directory') . "/assets/img" ?>/about_2.jpg" alt="мир детства">
+                <img class="aboutus__img" src="<?php echo get_field('history_img_2', 2)['url']; ?>" alt="<?php echo get_field('history_img_2', 2)['alt']; ?>">
             </div>
             <div class="col-lg-6">
                 <div class="subtitle">
-                    Главное - качество
+                    <?php the_field('history_sub_2', 2) ?>
                 </div>
-                <div class="aboutus__text">
-                    Мы делали их вручную, из лучших материалов и не жалея времени. Но мы росли и наш ассортимент расширился и фабричными изделиями.
-                    <br><br>
-                    Выбирая нас, вы можете быть уверены, что мы всегда следим за качеством закупок и никогда не предоставим вам опасный или некачественный товар.
-                </div>
+                <div class="aboutus__text"><?php the_field('history_descr_2', 2) ?></div>
             </div>
         </div>
 
         <div class="row">
             <div class="col-lg-6">
                 <div class="subtitle">
-                    Современные технологии
+                    <?php the_field('history_sub_3', 2) ?>
                 </div>
-                <div class="aboutus__text">
-                    И даже спустя столько лет мы продолжаем создавать игрушки вручную. Как самые простые, так и те, что идут в ногу со временем. Добавляя электроники и оживляя лучших друзей ребятишек, мы всегда следим за качеством и безопасностью. Каждая отдельная игрушка проходит индивидуальный контроль по всем необходимым стандартам.
-                    <br><br>
-                    Ведь счастливое лицо ребенка - это лучшая награда для нас!
-                </div>
+                <div class="aboutus__text"><?php the_field('history_descr_3', 2) ?></div>
             </div>
             <div class="col-lg-6">
-                <img class="aboutus__img" src="<?php echo bloginfo('template_directory') . "/assets/img" ?>/about_3.jpg" alt="мир детства">
+                <img class="aboutus__img" src="<?php echo get_field('history_img_3', 2)['url']; ?>" alt="<?php echo get_field('history_img_3', 2)['alt']; ?>">
             </div>
         </div>
     </div>
