2e328ea4c3b5a90c81922a62616ee79818c87a38
marat <savant.87@mail.ru>
2023-06-10 13:40:16 +0600

    replace smart toys static layout with get_posts();

--- index.php
+++ index.php
@@ -113,7 +113,7 @@
                                                                         if (has_post_thumbnail()) {
                                                                             the_post_thumbnail_url();
                                                                         } else {
-                                                                           echo get_theme_file_uri() . '/' . 'assets/img/not-found.jpg';
+                                                                            echo get_theme_file_uri() . '/' . 'assets/img/not-found.jpg';
                                                                         }
                                                                         ?>">
                     <div class="toys__item-info">
@@ -133,27 +133,39 @@
 
         <h2 class="subtitle">Развивающие игрушки</h2>
         <div class="toys__wrapper">
+            <?php
+            // параметры по умолчанию
+            $my_posts = get_posts(array(
+                'numberposts' => -1,
+                'category'    => 4,
+                'orderby'     => 'date',
+                'order'       => 'ASC',
+                'post_type'   => 'post',
+                'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+            ));
+            global $post;
 
-            <div class="toys__item" style="background-image: url(<?php echo bloginfo('template_directory') . "/assets/img" ?>/toy_7.jpg)">
-                <div class="toys__item-info">
-                    <div class="toys__item-title">Воздушный змей</div>
-                    <div class="toys__item-descr">
-                        Кто в детстве не хотел научиться летать? А змей поможет поймать ветер и унести все заботы далеко-далеко...
+            foreach ($my_posts as $post) {
+                setup_postdata($post); ?>
+                <div class="toys__item" style="background-image: url(<?php
+                                                                        if (has_post_thumbnail()) {
+                                                                            the_post_thumbnail_url();
+                                                                        } else {
+                                                                            echo get_theme_file_uri() . '/' . 'assets/img/not-found.jpg';
+                                                                        }
+                                                                        ?>">
+                    <div class="toys__item-info">
+                        <div class="toys__item-title"><?php the_title(); ?></div>
+                        <div class="toys__item-descr">
+                            <?php the_field('toy_description') ?>
+                        </div>
+                        <div class="minibutton toys__trigger">Подробнее</div>
                     </div>
-                    <div class="minibutton toys__trigger">Подробнее</div>
                 </div>
-            </div>
-
-            <div class="toys__item" style="background-image: url(<?php echo bloginfo('template_directory') . "/assets/img" ?>/toy_8.jpg)">
-                <div class="toys__item-info">
-                    <div class="toys__item-title">Музыкальные</div>
-                    <div class="toys__item-descr">
-                        Попробуйте заинтересовать ребенка музыкой! Может в нем таится будущий Джаред Лето!
-                    </div>
-                    <div class="minibutton toys__trigger">Подробнее</div>
-                </div>
-            </div>
-
+            <?php
+            }
+            wp_reset_postdata(); // сброс
+            ?>
         </div>
         <div class="row">
             <div class="col-lg-10 offset-lg-1">
